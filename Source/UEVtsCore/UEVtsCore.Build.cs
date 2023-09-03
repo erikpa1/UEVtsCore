0c09835d9ee49ec806a779318a99ff8358b20085
erik.palencik <erik.palencik@exe.sk>
2023-09-03 14:30:25 +0200

    Imgui remove and version upgrade

--- Source/UEVtsCore/UEVtsCore.Build.cs
+++ Source/UEVtsCore/UEVtsCore.Build.cs
@@ -8,7 +8,7 @@ public class UEVtsCore : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",  "ImGui" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
