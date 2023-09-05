5ee3209468b6f308deda899e5edf3cfc84555db4
erik.palencik <erik.palencik@exe.sk>
2023-09-05 21:11:56 +0200

    Update structs examples

---
+++ Source/UEVtsCore/Structures/Struct_Example.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+
+
+
+
+// UENUM()
+// enum class MyTestEnum : uint8
+// {
+// 	ValueA,
+// 	ValueB,
+// 	ValueC
+// };
+//
+//
+// USTRUCT()
+// struct FMyTestData : public FTableRowBase
+// {
+// 	GENERATED_BODY();
+// };
